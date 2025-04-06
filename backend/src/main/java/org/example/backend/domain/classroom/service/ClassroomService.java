a888ec2eec04f68bd4c8724aca29375de217eace
sewon <joosewon@naver.com>
2025-04-06 21:51:25 +0900

    :sparkles: (#17) classroom 생성하기 완료(create)

--- backend/src/main/java/org/example/backend/domain/classroom/service/ClassroomService.java
+++ backend/src/main/java/org/example/backend/domain/classroom/service/ClassroomService.java
@@ -1,4 +1,28 @@
 package org.example.backend.domain.classroom.service;
 
+import org.example.backend.domain.classroom.converter.ClassroomConverter;
+import org.example.backend.domain.classroom.dto.request.ClassroomRequestDTO;
+import org.example.backend.domain.classroom.entity.Classroom;
+import org.example.backend.domain.classroom.repository.ClassroomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ClassroomService {
+
+    private ClassroomRepository classroomRepository;
+    private ClassroomConverter classroomConverter;
+
+    @Autowired  // 의존성 주입이 필요합니다.
+    public ClassroomService(ClassroomConverter classroomConverter, ClassroomRepository classroomRepository) {
+        this.classroomConverter = classroomConverter;
+        this.classroomRepository = classroomRepository;
+    }
+
+// classroom 생성 로직
+    public Classroom createClassroom(ClassroomRequestDTO classroomRequestDTO) {
+        Classroom classroom = classroomConverter.toEntity(classroomRequestDTO);
+
+        return classroomRepository.save(classroom);
+    }
 }
