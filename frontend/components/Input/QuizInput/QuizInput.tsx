7956fcb183f1ab4171e5204e6e6e03bf17852d15
Son Ahyun <dkgus731@naver.com>
2025-04-09 16:06:10 +0900

    ✨ (#31) QuizInput 컴포넌트 추가 및 스타일 정의

---
+++ frontend/components/Input/QuizInput/QuizInput.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import React from "react";
+import styles from "./QuizInput.module.scss";
+
+// 입력 모드에 맞는 prop 타입을 정의
+type InputModeProps = {
+  mode: "input";
+  value: string; // 사용자가 입력한 값
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void; // 입력값 변경 핸들러
+};
+
+type ResultModeProps = {
+  mode: "result";
+  userAnswer: string; // 사용자 답변
+  correctAnswer: string; // 정답
+  count: number; // 선택한 사람 수
+};
+
+type TeacherModeProps = {
+  mode: "teacher";
+  correctAnswer: string; // 정답
+  count: number; // 선택한 사람 수
+};
+
+type QuizInputProps = InputModeProps | ResultModeProps | TeacherModeProps;
+
+const QuizInput: React.FC<QuizInputProps> = (props) => {
+  // props 타입 가드 함수
+  const isInputMode = (props: QuizInputProps): props is InputModeProps => {
+    return props.mode === "input";
+  };
+
+  const isResultMode = (props: QuizInputProps): props is ResultModeProps => {
+    return props.mode === "result";
+  };
+
+  const isTeacherMode = (props: QuizInputProps): props is TeacherModeProps => {
+    return props.mode === "teacher";
+  };
+
+  const getInputStyles = () => {
+    if (isInputMode(props)) {
+      return props.value ? styles.blueBorder : styles.grayBorder;
+    } else if (isResultMode(props)) {
+      return props.userAnswer === props.correctAnswer
+        ? styles.blueBorder
+        : styles.redBorder;
+    } else if (isTeacherMode(props)) {
+      return styles.blueBorder;
+    }
+    return styles.grayBorder;
+  };
+
+  const renderInputValue = () => {
+    if (isResultMode(props)) {
+      if (props.userAnswer === props.correctAnswer) {
+        return `${props.correctAnswer} (${props.count}명)`;
+      } else {
+        return props.userAnswer;
+      }
+    } else if (isTeacherMode(props)) {
+      return `정답 : ${props.correctAnswer} (${props.count}명)`;
+    } else if (isInputMode(props)) {
+      return props.value;
+    }
+    return "";
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <input
+        type="text"
+        value={renderInputValue()}
+        onChange={isInputMode(props) ? props.onChange : undefined}
+        className={`${styles.input} ${getInputStyles()}`}
+        placeholder="정답을 입력해주세요."
+        disabled={!isInputMode(props)}
+        readOnly={!isInputMode(props)}
+      />
+      {isResultMode(props) && props.userAnswer !== props.correctAnswer && (
+        <div className={styles.answerInfo}>
+          <p className={styles.correctAnswer}>정답: {props.correctAnswer}</p>
+          <p className={styles.count}>({props.count}명)</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default QuizInput;
