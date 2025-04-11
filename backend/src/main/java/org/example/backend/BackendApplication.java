778f7ad0472934a6503bab0964294e4687101149
sunninz <tnals00718@gmail.com>
2025-04-11 17:26:01 +0900

    ✨ (#35) SMTP 서버 연결

--- backend/src/main/java/org/example/backend/BackendApplication.java
+++ backend/src/main/java/org/example/backend/BackendApplication.java
@@ -21,6 +21,10 @@ public class BackendApplication {
 		System.setProperty("REDIS_HOST", dotenv.get("REDIS_HOST"));
 		System.setProperty("REDIS_PORT", dotenv.get("REDIS_PORT"));
 		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+		System.setProperty("MAIL_HOST", dotenv.get("MAIL_HOST"));
+		System.setProperty("MAIL_PORT", dotenv.get("MAIL_PORT"));
+		System.setProperty("MAIL_USERNAME", dotenv.get("MAIL_USERNAME"));
+		System.setProperty("MAIL_PASSWORD", dotenv.get("MAIL_PASSWORD"));
 
 		SpringApplication.run(BackendApplication.class, args);
 	}
--- backend/src/main/java/org/example/backend/global/redis/RedisConfig.java
+++ backend/src/main/java/org/example/backend/global/redis/RedisConfig.java
@@ -6,6 +6,7 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
@@ -22,9 +23,11 @@ public class RedisConfig {
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
+    public RedisTemplate<String, String> redisTemplate() {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory()); // redis 서버와 연결
+        redisTemplate.setKeySerializer(new StringRedisSerializer()); // key를 문자열로 직렬화
+        redisTemplate.setValueSerializer(new StringRedisSerializer()); // value를 문자열로 직렬화
         return redisTemplate;
     }
 }
