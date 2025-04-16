7cc423826914efad361c70246012ffd549687ad2
Son Ahyun <dkgus731@naver.com>
2025-04-16 15:40:09 +0900

    ✨ (#58) 학생 레이아웃에 페이지 타이틀 헤더 추가 및 네비게이션 표시 기능 개선

--- frontend/app/student/layout.tsx
+++ frontend/app/student/layout.tsx
@@ -2,6 +2,7 @@
 import { usePathname } from "next/navigation";
 import Navigation from "@/components/Navigation/Navigation";
 import { ROUTES } from "@/constants/routes";
+import StudentPageTitleHeader from "@/components/Header/StudentPageTitleHeader/StudentPageTitleHeader";
 
 export default function StudentLayout({
   children,
@@ -18,10 +19,33 @@ export default function StudentLayout({
     ROUTES.studentProfile,
   ];
 
+  const titleHeaderPages = [
+    ROUTES.studentClass,
+    ROUTES.studentNotification,
+    ROUTES.studentProfile,
+  ];
+
+  // 각 페이지에 대한 타이틀 설정
+  const pageTitles: { [key: string]: string } = {
+    [ROUTES.studentClass]: "클래스",
+    [ROUTES.studentNotification]: "알림",
+    [ROUTES.studentProfile]: "프로필",
+    // 추가적인 경로 및 타이틀을 설정할 수 있습니다
+  };
+
   // 네비게이션을 해당 경로에서만 표시
   const showNavigation = navigationPages.includes(pathname);
+  const showTitleHeader = titleHeaderPages.includes(pathname);
+
+  const pageTitle = pageTitles[pathname] || "기본 타이틀"; // 기본 타이틀을 설정할 수도 있습니다.
+
   return (
-    <body className={`student-body ${showNavigation ? "show-nav" : ""}`}>
+    <body
+      className={`student-body ${showNavigation ? "show-nav" : ""} ${
+        showTitleHeader ? "show-header" : ""
+      }`}
+    >
+      {showTitleHeader && <StudentPageTitleHeader title={pageTitle} />}
       {children}
       {showNavigation && <Navigation />}
     </body>
