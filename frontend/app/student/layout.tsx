d499289a6518e1611bae834c6e450ec0c70523d2
Son Ahyun <dkgus731@naver.com>
2025-04-16 13:50:53 +0900

    💄 (#58) 학생 레이아웃에 네비게이션 표시 기능 추가 및 스타일 수정

--- frontend/app/student/layout.tsx
+++ frontend/app/student/layout.tsx
@@ -1,7 +1,27 @@
+"use client";
+import { usePathname } from "next/navigation";
+import Navigation from "@/components/Navigation/Navigation";
 export default function StudentLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <body className="student-body">{children}</body>;
+  const pathname = usePathname(); // 현재 경로
+
+  // 네비게이션을 표시할 페이지 경로 목록
+  const navigationPages = [
+    "/student", // studentHome
+    "/student/class", // studentClass
+    "/student/notification", // studentNotification
+    "/student/profile", // studentProfile
+  ];
+
+  // 네비게이션을 해당 경로에서만 표시
+  const showNavigation = navigationPages.includes(pathname);
+  return (
+    <body className={`student-body ${showNavigation ? "show-nav" : ""}`}>
+      {children}
+      {showNavigation && <Navigation />}
+    </body>
+  );
 }
