10339ee9c0b5ddd64ca7bd01e7460b435d5bff66
sunninz <tnals00718@gmail.com>
2025-04-20 14:45:00 +0900

    ✨ (#59) 토큰 만료 에러 처리, 개발환경 인증 생략

--- backend/src/main/java/org/example/backend/global/security/JWTFilter.java
+++ backend/src/main/java/org/example/backend/global/security/JWTFilter.java
@@ -11,6 +11,7 @@ import org.example.backend.domain.user.entity.User;
 import org.example.backend.domain.user.exception.UserErrorCode;
 import org.example.backend.domain.user.service.CustomUserDetails;
 import org.example.backend.global.ApiResponse;
+import org.example.backend.global.code.base.FailureCode;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -35,6 +36,12 @@ public class JWTFilter extends OncePerRequestFilter {
             return;
         }
 
+        // 개발 중 인증 생략
+        if (uri.startsWith("/api/lectures")|| uri.startsWith("/api/classes")|| uri.startsWith("/api/notifications")|| uri.startsWith("/api/quizzes")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         // request에서 Authorization 헤더를 찾음
         String authorization = request.getHeader("Authorization");
 
@@ -49,18 +56,12 @@ public class JWTFilter extends OncePerRequestFilter {
 
         // Bearer 제외하고 토큰만 획득
         String token = authorization.substring(7);
-        System.out.println(token);
 
         // 토큰 소멸 시간 검증
-        if(jwtUtil.isExpired(token)){
-            System.out.println("token expired");
+        try {
 
-            // 조건에 해당되면 메소드 종료
-            return;
-        }
-
-        // 토큰에서 email, username, role 획득
-        String email, roleString, name;
+            // 토큰에서 email, username, role 획득
+            String email, roleString, name;
             email = jwtUtil.getEmail(token);
             roleString = jwtUtil.getRole(token);
             name = jwtUtil.getName(token);
@@ -75,19 +76,38 @@ public class JWTFilter extends OncePerRequestFilter {
             user.setRole(role);
 
             // UserDetail에 회원정보 객체 담기
-            CustomUserDetails customUserDetails = new CustomUserDetails(user,accountLocal);
+            CustomUserDetails customUserDetails = new CustomUserDetails(user, accountLocal);
 
             // 스프링 시큐리티 인증 토큰 생성
-            Authentication authToken = new UsernamePasswordAuthenticationToken(customUserDetails,null,customUserDetails.getAuthorities());
+            Authentication authToken = new UsernamePasswordAuthenticationToken(customUserDetails, null, customUserDetails.getAuthorities());
 
             // 세션에 사용자 등록
             SecurityContextHolder.getContext().setAuthentication(authToken);
 
             filterChain.doFilter(request, response);
 
+        }catch(io.jsonwebtoken.ExpiredJwtException e){
+            // 토큰 만료
+            setErrorResponse(response, UserErrorCode._TOKEN_EXPIRED);
+        } catch(Exception e){
+            // 인증 실패
+            setErrorResponse(response, FailureCode._UNAUTHORIZED);
+        }
+
+    }
+    // UserError 타입
+    private void setErrorResponse(HttpServletResponse response, UserErrorCode errorCode) throws IOException {
+        response.setStatus(errorCode.getReasonHttpStatus().getHttpStatus().value());
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        ApiResponse<Object> apiResponse = ApiResponse.onFailure(errorCode);
+        String json = new ObjectMapper().writeValueAsString(apiResponse);
+        response.getWriter().write(json);
     }
 
-    private void setErrorResponse(HttpServletResponse response, UserErrorCode errorCode) throws IOException {
+    //  FailureCode 타입
+    private void setErrorResponse(HttpServletResponse response, FailureCode errorCode) throws IOException {
         response.setStatus(errorCode.getReasonHttpStatus().getHttpStatus().value());
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
