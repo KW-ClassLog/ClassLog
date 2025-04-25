0b54b438183aecad038d97579b79b955a56f6f1d
Son Ahyun <dkgus731@naver.com>
2025-04-25 11:56:31 +0900

    🔨 (#76) ApiResponse 인터페이스의 result 속성을 선택적으로 변경

--- frontend/types/apiResponseTypes.ts
+++ frontend/types/apiResponseTypes.ts
@@ -2,5 +2,5 @@ export interface ApiResponse<T = unknown> {
   isSuccess: boolean;
   code: string;
   message?: string;
-  result: T; // string, number, boolean, Object, ... 다양한 타입이 올 수 있으므로
+  result?: T; // string, number, boolean, Object, ... 다양한 타입이 올 수 있으므로
 }
