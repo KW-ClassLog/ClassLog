fed4bde20e71c2ba00c557dfa1afecb25595aaa1
sunninz <tnals00718@gmail.com>
2025-05-10 16:02:16 +0900

    :sparkles: (#100) user 엔티티 수정

---
+++ backend/src/main/java/org/example/backend/domain/user/entity/SocialType.java
@@ -0,0 +1,5 @@
+package org.example.backend.domain.user.entity;
+
+public enum SocialType {
+    LOCAL, KAKAO
+}
---
+++ backend/src/main/java/org/example/backend/domain/user/entity/Status.java
@@ -0,0 +1,5 @@
+package org.example.backend.domain.user.entity;
+
+public enum Status {
+    ACTIVE, INACTIVE
+}
