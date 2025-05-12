6d46ba110cb3cd084c353748676920923cf76a44
sewon <joosewon@naver.com>
2025-05-12 16:13:08 +0900

    :recycle: (#102) 학생일 때 클래스 못만들도록 함

--- backend/src/main/java/org/example/backend/domain/classroom/service/ClassroomServiceImpl.java
+++ backend/src/main/java/org/example/backend/domain/classroom/service/ClassroomServiceImpl.java
@@ -10,6 +10,7 @@ import org.example.backend.domain.lecture.entity.Lecture;
 import org.example.backend.domain.lecture.repository.LectureRepository;
 import org.example.backend.domain.lectureNote.entity.LectureNote;
 import org.example.backend.domain.lectureNote.repository.LectureNoteRepository;
+import org.example.backend.domain.user.entity.Role;
 import org.example.backend.global.S3.service.S3Service;
 import org.example.backend.global.security.auth.CustomUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -45,6 +46,11 @@ public class ClassroomServiceImpl implements ClassroomService {
         Object principal = authentication.getPrincipal();
 
         UUID userId = ((CustomUserDetails) principal).getUser().getId();
+        Role role =  ((CustomUserDetails) principal).getUser().getRole();
+
+        if (role == Role.STUDENT) {
+            throw new ClassroomException(ClassroomErrorCode.STUDENT_CANNOT_CREATE_CLASSROOM);
+        }
 
         Classroom classroom = classroomConverter.toEntity(classroomRequestDTO, userId);
 
