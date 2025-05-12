84ff0078b5e2fe6fd59be8c62d751127f99b5010
sunninz <tnals00718@gmail.com>
2025-05-12 13:23:15 +0900

    :recycle: (#100) accountLocal import 삭제

--- backend/src/main/java/org/example/backend/domain/user/converter/UserConverter.java
+++ backend/src/main/java/org/example/backend/domain/user/converter/UserConverter.java
@@ -1,7 +1,6 @@
 package org.example.backend.domain.user.converter;
 
 
-import org.example.backend.domain.accountLocal.entity.AccountLocal;
 import org.example.backend.domain.user.dto.request.RegisterRequestDTO;
 import org.example.backend.domain.user.entity.SocialType;
 import org.example.backend.domain.user.entity.Status;
@@ -10,7 +9,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
-public class UserConverter {
+public class
+UserConverter {
 
     public User toUser(RegisterRequestDTO registerRequestDTO, PasswordEncoder passwordEncoder){
 
