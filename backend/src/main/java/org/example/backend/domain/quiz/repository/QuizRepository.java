14e3ddd13e9ce13018492831550881ac97556099
Haemin Kim <haemin1230@gmail.com>
2025-05-26 14:49:00 +0900

    :sparkles: (#115) OptionRepository, QuizRepository 추가

--- backend/src/main/java/org/example/backend/domain/quiz/repository/QuizRepository.java
+++ backend/src/main/java/org/example/backend/domain/quiz/repository/QuizRepository.java
@@ -1,4 +1,8 @@
 package org.example.backend.domain.quiz.repository;
 
-public interface QuizRepository {
+import org.example.backend.domain.quiz.entity.Quiz;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.UUID;
+
+public interface QuizRepository extends JpaRepository<Quiz, UUID> {
 }
