d2ad5f83b940bd213dc9653d388d5c8dbdec6d3b
sewon <joosewon@naver.com>
2025-05-28 15:20:05 +0900

    :sparkles: (#92) 양방향 매핑 삭제 추가 lectureNoteMappingArrayList

--- backend/src/main/java/org/example/backend/domain/lectureNote/entity/LectureNote.java
+++ backend/src/main/java/org/example/backend/domain/lectureNote/entity/LectureNote.java
@@ -2,9 +2,13 @@ package org.example.backend.domain.lectureNote.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import org.example.backend.domain.classroom.entity.Classroom;
+import org.example.backend.domain.lectureNoteMapping.entity.LectureNoteMapping;
 import org.example.backend.global.entitiy.BaseEntity;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
@@ -30,4 +34,7 @@ public class LectureNote extends BaseEntity {
     @OnDelete(action= OnDeleteAction.CASCADE)
     @JoinColumn(name = "class_id", nullable = false)
     private Classroom classroom;
+
+    @OneToMany(mappedBy = "lectureNote", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<LectureNoteMapping> lectureNoteMappingArrayList = new ArrayList<>();
 }
