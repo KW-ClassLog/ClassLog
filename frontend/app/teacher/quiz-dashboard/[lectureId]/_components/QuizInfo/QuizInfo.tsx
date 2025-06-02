76381ce675cc27e19e0d24c58688aad1b6d70f9e
Son Ahyun <dkgus731@naver.com>
2025-06-03 02:41:34 +0900

    ✨ (#124) 퀴즈 정보 컴포넌트 구현 및 스타일 추가

--- frontend/app/teacher/quiz-dashboard/[lectureId]/_components/QuizInfo/QuizInfo.tsx
+++ frontend/app/teacher/quiz-dashboard/[lectureId]/_components/QuizInfo/QuizInfo.tsx
@@ -1,6 +1,27 @@
 "use client";
 import React from "react";
 import styles from "./QuizInfo.module.scss";
+
+const data = {
+  title: "강의제목입니다~~",
+  quizDate: "2025-04-14",
+  quizDay: "월",
+};
+
+function formatDate(date: string, day: string) {
+  const [yyyy, mm, dd] = date.split("-");
+  return `${yyyy}.${mm}.${dd} (${day})`;
+}
+
 export default function QuizInfo() {
-  return <div>퀴즈 정보</div>;
+  return (
+    <div className={styles.infoRow}>
+      <div className={styles.title}>
+        [{data.title}] <span className={styles.dashboard}>퀴즈 대시보드</span>
+      </div>
+      <div className={styles.date}>
+        {formatDate(data.quizDate, data.quizDay)}
+      </div>
+    </div>
+  );
 }
