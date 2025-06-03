6aef3168ed0af1040d12ac0e0df1f2c84ede2414
Son Ahyun <dkgus731@naver.com>
2025-06-03 12:59:56 +0900

    ✨ (#154) 강의 생성 API 및 타입 추가

---
+++ frontend/api/classes/createClass.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { axiosInstance } from "@/api/axiosInstance";
+import { ENDPOINTS } from "@/constants/endpoints";
+import { ApiResponse } from "@/types/apiResponseTypes";
+import {
+  CreateClassRequest,
+  CreateClassResult,
+} from "@/types/classes/createClassTypes";
+
+export async function createClass({
+  className,
+  classDate,
+  startDate,
+  endDate,
+}: CreateClassRequest) {
+  try {
+    const response = await axiosInstance.post<
+      ApiResponse<CreateClassResult | null>
+    >(ENDPOINTS.USERS.SIGNUP, { className, classDate, startDate, endDate });
+    return response.data;
+  } catch (error: unknown) {
+    if (axios.isAxiosError(error) && error.response) {
+      return error.response.data as ApiResponse<CreateClassResult>;
+    }
+    throw error;
+  }
+}
