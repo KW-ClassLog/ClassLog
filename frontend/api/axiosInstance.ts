eaa7b78ba3b235bf595bcb767668262bb75467e8
Son Ahyun <dkgus731@naver.com>
2025-06-03 13:50:05 +0900

    🗑️ (#154) axios 인스턴스에서 caseConverter 관련 코드 및 파일 삭제

--- frontend/api/axiosInstance.ts
+++ frontend/api/axiosInstance.ts
@@ -1,34 +1,6 @@
 import axios from "axios";
-import { toSnakeCase, toCamelCase } from "@/utils/caseConverter";
 
 export const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   withCredentials: true,
 });
-
-// 요청 보내기 전에 -> camelCase → snake_case
-axiosInstance.interceptors.request.use((config) => {
-  if (config.data) {
-    config.data = toSnakeCase(config.data);
-  }
-  if (config.params) {
-    config.params = toSnakeCase(config.params);
-  }
-  return config;
-});
-
-// 응답 받을 때 -> snake_case → camelCase
-axiosInstance.interceptors.response.use(
-  (response) => {
-    if (response.data) {
-      response.data = toCamelCase(response.data);
-    }
-    return response;
-  },
-  (error) => {
-    if (error.response && error.response.data) {
-      error.response.data = toCamelCase(error.response.data);
-    }
-    return Promise.reject(error);
-  }
-);
