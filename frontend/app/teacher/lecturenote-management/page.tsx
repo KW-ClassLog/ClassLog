f179c0542fc5fea3422ddc0a769b009b2514c11d
Son Ahyun <dkgus731@naver.com>
2025-06-04 13:08:37 +0900

    ✨ (#167) 강의자료 관리 페이지 스타일 및 구조 추가, 선택된 클래스가 없을 때의 NoDataView 추가

--- frontend/app/teacher/lecturenote-management/page.tsx
+++ frontend/app/teacher/lecturenote-management/page.tsx
@@ -1,7 +1,14 @@
 "use client";
 import ManagementTable from "@/components/ManagementTable/ManagementTable";
+import styles from "./page.module.scss";
+import useSelectedClassStore from "@/store/useSelectedClassStore";
+import NoDataView from "@/components/NoDataView/NoDataView";
+import { FileText } from "lucide-react";
+import LoadingSpinner from "@/components/LoadingSpinner/LoadingSpinner";
 
 export default function TeacherLectureNoteManagementPage() {
+  const { selectedClassId, selectedClassName } = useSelectedClassStore();
+
   const lectureNotes = [
     {
       lectureNoteId: "note1",
@@ -27,13 +34,40 @@ export default function TeacherLectureNoteManagementPage() {
     console.log("Updated Lecture Notes:", updatedLectureNotes);
   };
 
+  if (!selectedClassId || !selectedClassName) {
+    return (
+      <div className={styles.container}>
+        <h1>퀴즈 관리</h1>
+        <NoDataView
+          icon={FileText}
+          title="선택된 클래스가 없습니다"
+          description="좌상단의 클래스 선택 메뉴에서 클래스를 선택해주세요"
+        />
+      </div>
+    );
+  }
+  // if (isLoading) {
+  //   return (
+  //     <div className={styles.container}>
+  //       <h1>[{selectedClassName}] 강의 관리</h1>
+  //       <LoadingSpinner />
+  //     </div>
+  //   );
+  // }
+
   return (
-    <div style={{ padding: "20px" }}>
-      <ManagementTable
-        type="lectureNote"
-        data={lectureNotes}
-        onDelete={handleDelete}
-      />
+    <div className={styles.container}>
+      <h1>[{selectedClassName}] 강의자료 관리</h1>
+      <div className={styles.addButtonContainer}>
+        <button className={styles.addButton}>+ 강의자료 추가하기</button>
+      </div>
+      <div className={styles.lectureNoteListContainer}>
+        <ManagementTable
+          type="lectureNote"
+          data={lectureNotes}
+          onDelete={handleDelete}
+        />
+      </div>
     </div>
   );
 }
