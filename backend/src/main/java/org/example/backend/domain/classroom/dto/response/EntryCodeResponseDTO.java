e3f3f9a45fb821dbf39ebd5496f3c98fed8a49e0
sewon <joosewon@naver.com>
2025-07-08 22:45:35 +0900

    :recycle: (#246) 카멜 케이스로 변경

--- backend/src/main/java/org/example/backend/domain/classroom/dto/response/EntryCodeResponseDTO.java
+++ backend/src/main/java/org/example/backend/domain/classroom/dto/response/EntryCodeResponseDTO.java
@@ -7,7 +7,7 @@ import java.util.UUID;
 @Getter
 @AllArgsConstructor
 public class EntryCodeResponseDTO {
-    private UUID class_id;
-    private String entry_code;
-    private String expires_at;
+    private UUID classId;
+    private String entryCode;
+    private String expiresAt;
 }
