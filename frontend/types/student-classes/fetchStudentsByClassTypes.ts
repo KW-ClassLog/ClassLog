f20b9678a69f98df4011bb2e2804a308102f80d0
Son Ahyun <dkgus731@naver.com>
2025-07-09 16:38:41 +0900

    🔧 (#241) 학생 관리 테이블에서 데이터 타입을 FetchStudentsByClassResult로 변경하고, 관련된 코드 수정

--- frontend/types/student-classes/fetchStudentsByClassTypes.ts
+++ frontend/types/student-classes/fetchStudentsByClassTypes.ts
@@ -1,8 +1,8 @@
 export interface FetchStudentsByClassResult {
   userId: string;
   name: string;
-  nickname: string;
+  nickname: string | null;
   phoneNumber: string;
-  profileUrl: string;
+  profileUrl: string | null;
   organization: string;
 }
