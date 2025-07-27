8f57c8eb5dfcbd6077b68c43bd565fdaa295447c
Haemin Kim <haemin1230@gmail.com>
2025-07-27 15:51:56 +0900

    :sparkles: (#252) 퀴즈 문제 조회 로직 구현

--- backend/src/main/java/org/example/backend/domain/option/dto/response/OptionResponseDTO.java
+++ backend/src/main/java/org/example/backend/domain/option/dto/response/OptionResponseDTO.java
@@ -1,28 +1,14 @@
 package org.example.backend.domain.option.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 import java.util.UUID;
 
-public static class OptionResponseDTO {
+@Getter
+@AllArgsConstructor
+public class OptionResponseDTO {
     private UUID id;
     private int optionOrder;
     private String text;
-
-    public OptionResponseDTO(UUID id, int optionOrder, String text) {
-        this.id = id;
-        this.optionOrder = optionOrder;
-        this.text = text;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public int getOptionOrder() {
-        return optionOrder;
-    }
-
-    public String getText() {
-        return text;
-    }
-}
 }
--- backend/src/main/java/org/example/backend/domain/quiz/dto/response/QuizListResponseDTO.java
+++ backend/src/main/java/org/example/backend/domain/quiz/dto/response/QuizListResponseDTO.java
@@ -1,28 +1,23 @@
 package org.example.backend.domain.quiz.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 import org.example.backend.domain.option.dto.response.OptionResponseDTO;
 
 import java.util.List;
 import java.util.UUID;
 
+@Getter
+@AllArgsConstructor
+@Builder
 public class QuizListResponseDTO {
-
     private UUID lectureId;
     private List<QuizDTO> quizzes;
 
-    public QuizListResponseDTO(UUID lectureId, List<QuizDTO> quizzes) {
-        this.lectureId = lectureId;
-        this.quizzes = quizzes;
-    }
-
-    public UUID getLectureId() {
-        return lectureId;
-    }
-
-    public List<QuizDTO> getQuizzes() {
-        return quizzes;
-    }
-
+    @Getter
+    @AllArgsConstructor
+    @Builder
     public static class QuizDTO {
         private UUID quizId;
         private int quizOrder;
@@ -30,38 +25,5 @@ public class QuizListResponseDTO {
         private String solution;
         private String type;
         private List<OptionResponseDTO> options;
-
-        public QuizDTO(UUID quizId, int quizOrder, String quizBody, String solution, String type, List<OptionResponseDTO> options) {
-            this.quizId = quizId;
-            this.quizOrder = quizOrder;
-            this.quizBody = quizBody;
-            this.solution = solution;
-            this.type = type;
-            this.options = options;
-        }
-
-        public UUID getQuizId() {
-            return quizId;
-        }
-
-        public int getQuizOrder() {
-            return quizOrder;
-        }
-
-        public String getQuizBody() {
-            return quizBody;
-        }
-
-        public String getSolution() {
-            return solution;
-        }
-
-        public String getType() {
-            return type;
-        }
-
-        public List<OptionResponseDTO> getOptions() {
-            return options;
-        }
     }
 }
