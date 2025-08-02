0ba9786c0298ba7f0d7bfe5250e3da45631a6243
sunninz <tnals00718@gmail.com>
2025-08-02 22:26:15 +0900

    :sparkles: (#266) 질문 엔티티 생성

--- backend/src/main/java/org/example/backend/domain/question/entity/Question.java
+++ backend/src/main/java/org/example/backend/domain/question/entity/Question.java
@@ -1,4 +1,42 @@
 package org.example.backend.domain.question.entity;
 
-public class Question {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.backend.domain.lecture.entity.Lecture;
+import org.example.backend.domain.user.entity.User;
+import org.example.backend.global.entitiy.BaseEntity;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "question")
+public class Question extends BaseEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(columnDefinition = "BINARY(16)")
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "lecture_id", nullable = false)
+    private Lecture lecture;
+
+    @Column(name = "content", nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @Column(name = "timestamp", nullable = false)
+    private LocalDateTime timestamp;
 }
