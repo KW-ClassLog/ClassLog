2a5697213015e2eba2fc523ba9492d66ee6fdae1
Son Ahyun <dkgus731@naver.com>
2025-08-16 14:42:16 +0900

    ✨ (#280) 클래스 정보 컴포넌트 추가 및 api 연결

---
+++ frontend/app/student/class-detail/[classId]/_components/LectureListAndNote/LectureListAndNote.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./LectureListAndNote.module.scss";
+
+export default function LectureListAndNote() {
+  return <div className={styles.container}>강의 목록 및 자료</div>;
+}
