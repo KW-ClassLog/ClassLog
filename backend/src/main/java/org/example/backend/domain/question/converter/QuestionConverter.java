edf1c2d61aba6621a27cfc5504be2397002563af
sunninz <tnals00718@gmail.com>
2025-08-22 03:30:17 +0900

    :sparkles: (#297) question 도메인 clodfront 조회 방식으로 변경

--- backend/src/main/java/org/example/backend/domain/question/converter/QuestionConverter.java
+++ backend/src/main/java/org/example/backend/domain/question/converter/QuestionConverter.java
@@ -3,19 +3,15 @@ package org.example.backend.domain.question.converter;
 import lombok.RequiredArgsConstructor;
 import org.example.backend.domain.question.dto.response.QuestionResponseDTO;
 import org.example.backend.domain.question.entity.Question;
-import org.example.backend.domain.user.entity.SocialType;
 import org.example.backend.global.S3.service.S3Service;
 import org.springframework.stereotype.Component;
+
 
 @Component
 @RequiredArgsConstructor
 public class QuestionConverter {
 
-    private final S3Service s3Service;
-
-    public QuestionResponseDTO.teacher toTeacherQuestions(Question question) {
-        String profileUrl = question.getUser().getSocialType() == SocialType.LOCAL?
-                s3Service.getPublicUrl(question.getUser().getProfileUrl()) : question.getUser().getProfileUrl();
+    public QuestionResponseDTO.teacher toTeacherQuestions(Question question, String profileUrl){
 
         return QuestionResponseDTO.teacher.builder()
                 .studentId(question.getUser().getId())
--- backend/src/main/java/org/example/backend/domain/question/service/QuestionServiceImpl.java
+++ backend/src/main/java/org/example/backend/domain/question/service/QuestionServiceImpl.java
@@ -7,9 +7,18 @@ import org.example.backend.domain.question.exception.QuestionErrorCode;
 import org.example.backend.domain.question.exception.QuestionException;
 import org.example.backend.domain.question.repository.QuestionRepository;
 import org.example.backend.domain.studentClass.repository.StudentClassRepository;
+import org.example.backend.domain.user.entity.SocialType;
 import org.example.backend.domain.user.entity.User;
+import org.example.backend.domain.user.exception.UserErrorCode;
+import org.example.backend.domain.user.exception.UserException;
+import org.example.backend.domain.user.repository.UserRepository;
+import org.example.backend.global.S3.service.S3Service;
+import org.example.backend.global.code.base.FailureCode;
+import org.example.backend.global.exception.GeneralException;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.security.spec.InvalidKeySpecException;
 import java.util.List;
 import java.util.UUID;
 
@@ -20,12 +29,30 @@ public class QuestionServiceImpl implements QuestionService {
     private final QuestionRepository questionRepository;
     private final StudentClassRepository studentClassRepository;
     private final QuestionConverter questionConverter;
+    private final S3Service s3Service;
+    private final UserRepository userRepository;
 
     @Override
-    public List<QuestionResponseDTO.teacher> getTeacherQuestions(UUID lectureId, User user ) {
+    public List<QuestionResponseDTO.teacher> getTeacherQuestions(UUID lectureId, User user) {
 
         return questionRepository.findByLectureId(lectureId).stream()
-                .map(questionConverter::toTeacherQuestions)
+                .map(question -> {
+                    User student = userRepository.findById(question.getUser().getId())
+                            .orElseThrow(() -> new UserException(UserErrorCode._USER_NOT_FOUND));
+
+                    String profileUrl;
+
+                    if (student.getProfileUrl() != null && user.getSocialType() == SocialType.LOCAL) {
+                        try {
+                            profileUrl = s3Service.getSignedUrl(user.getProfileUrl());
+                        } catch (IOException | InvalidKeySpecException e) {
+                            throw new GeneralException(FailureCode._INTERNAL_SERVER_ERROR);
+                        }
+                    } else {
+                        profileUrl = student.getProfileUrl();
+                    }
+                    return questionConverter.toTeacherQuestions(question, profileUrl);
+                })
                 .toList();
     }
 
