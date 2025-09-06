095265684e4be4385217419b135011f96e46f88d
Son Ahyun <dkgus731@naver.com>
2025-09-06 09:35:12 +0900

    🔨 (#305) 헤더에서 lectureTitle이 아닌 classTitle 저장 후 불러오기

--- frontend/app/student/lecture-detail/[lectureId]/page.tsx
+++ frontend/app/student/lecture-detail/[lectureId]/page.tsx
@@ -8,11 +8,15 @@ import LecrureRecordSection from "./_components/LecrureRecordSection/LecrureReco
 import QuizSection from "./_components/QuizSection/QuizSection";
 import style from "./page.module.scss";
 import Tab from "@/components/Tab/Tab";
+import { useClassTitleStore } from "@/store/useClassTitleStore";
 
 export default function StudentLectureDetailPage() {
   const params = useParams();
+  const { setClassTitle } = useClassTitleStore();
   const lectureId = params.lectureId as string;
   const [selectedTab, setSelectedTab] = useState(0);
+
+  // TODO: lectureId로 클래스타이틀 불러오고, 이를 setClassTitle 이용해서 스토어에 저장해야 함
 
   const tabs = ["수업 자료", "질문하기", "강의 녹음", "복습 퀴즈"];
 
