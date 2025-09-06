88c00b24993d6a4bb07fc603ce7119d8c15b73e7
Son Ahyun <dkgus731@naver.com>
2025-09-06 09:20:34 +0900

    ✨ (#305) 강의 상세 페이지 및 정보 섹션 컴포넌트 추가, 스타일 적용

--- frontend/app/student/lecture-detail/[lectureId]/page.tsx
+++ frontend/app/student/lecture-detail/[lectureId]/page.tsx
@@ -1,3 +1,14 @@
+"use client";
+import { useParams } from "next/navigation";
+import LectureInfoSection from "./_components/LectureInfoSection/LectureInfoSection";
+import style from "./page.module.scss";
+
 export default function StudentLectureDetailPage() {
-  return <div>강의 상세</div>;
+  const params = useParams();
+  const lectureId = params.lectureId as string;
+  return (
+    <div className={style.lectureDetailPage}>
+      <LectureInfoSection lectureId={lectureId} />
+    </div>
+  );
 }
