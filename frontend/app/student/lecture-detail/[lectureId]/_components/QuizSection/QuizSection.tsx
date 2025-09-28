1ed5dd3d1432731519dbff415148b8ccbcfcff9f
Son Ahyun <dkgus731@naver.com>
2025-09-28 14:09:14 +0900

    ✨ (#305)강의 상태에 따른 퀴즈 상태 관리 로직 추가

--- frontend/app/student/lecture-detail/[lectureId]/_components/QuizSection/QuizSection.tsx
+++ frontend/app/student/lecture-detail/[lectureId]/_components/QuizSection/QuizSection.tsx
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useLectureStatusStore } from "@/store/useLectureStatusStore";
+import dayjs from "dayjs";
 
 interface QuizSectionProps {
   lectureId: string;
 }
 
+export type QuizStatus = "notYet" | "solve" | "waitingResult" | "viewResult";
+
 export default function QuizSection({ lectureId }: QuizSectionProps) {
   const { lectureStatus } = useLectureStatusStore();
+  const [quizStatus, setQuizStatus] = useState<QuizStatus>("notYet");
+
+  useEffect(() => {
+    const canViewResult = () => {
+      const now = dayjs(
+        new Date(new Date().toLocaleString("en-US", { timeZone: "Asia/Seoul" }))
+      );
+      const midnight = now.startOf("day").add(1, "day");
+      return now.isAfter(midnight);
+    };
+    switch (lectureStatus) {
+      case "beforeLecture":
+      case "onLecture":
+      case "afterLectureBeforeQuiz":
+        setQuizStatus("notYet");
+        break;
+      case "quizReadyForSubmission":
+        setQuizStatus("solve");
+        break;
+      case "viewMyQuizResult":
+        if (canViewResult()) {
+          setQuizStatus("viewResult");
+        } else {
+          setQuizStatus("waitingResult");
+        }
+        break;
+    }
+  }, [lectureStatus]);
 
   return (
     <div>
