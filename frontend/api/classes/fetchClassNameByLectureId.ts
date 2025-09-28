b02995a5380ed87640e4c2a3601d5b157b21e047
Son Ahyun <dkgus731@naver.com>
2025-09-28 16:46:10 +0900

    ✨ (#305) 강의 상세 페이지에서 lectureId를 기반으로 클래스 이름을 불러오는 API 함수 추가 및 관련 로직 구현

---
+++ frontend/api/classes/fetchClassNameByLectureId.ts
@@ -0,0 +1,20 @@
+import { axiosInstance } from "@/api/axiosInstance";
+import axios from "axios";
+import { ENDPOINTS } from "@/constants/endpoints";
+import { ApiResponse } from "@/types/apiResponseTypes";
+import { FetchClassNameByLectureIdResult } from "@/types/classes/fetchClassNameByLectureIdTypes";
+
+export async function fetchClassNameByLectureId(lectureId: string) {
+  try {
+    const response = await axiosInstance.get<
+      ApiResponse<FetchClassNameByLectureIdResult>
+    >(ENDPOINTS.CLASSES.GET_CLASS_NAME(lectureId));
+    return response.data;
+  } catch (error: unknown) {
+    if (axios.isAxiosError(error) && error.response) {
+      return error.response
+        .data as ApiResponse<FetchClassNameByLectureIdResult>;
+    }
+    throw error;
+  }
+}
