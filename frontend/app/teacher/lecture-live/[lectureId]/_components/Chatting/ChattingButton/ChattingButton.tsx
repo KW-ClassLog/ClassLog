f11ff1ee12d9e0bcea1c13b1e5ee05a0080cb9b5
Haemin Kim <haemin1230@gmail.com>
2025-10-07 00:50:47 +0900

    📦 (#336) 채팅 파일명 변경

--- frontend/app/teacher/lecture-live/[lectureId]/_components/Chatting/ChattingButton/ChattingButton.tsx
+++ frontend/app/teacher/lecture-live/[lectureId]/_components/Chatting/ChattingButton/ChattingButton.tsx
@@ -4,9 +4,9 @@ import { useEffect, useState } from "react";
 import IconButton from "@/components/Button/IconButton/IconButton";
 import { MessageCircleMore } from "lucide-react";
 import { useLive } from "../../LectureLiveProvider";
-import styles from "./ChatingButton.module.scss";
+import styles from "./chattingButton.module.scss";
 
-export default function ChatingButton({
+export default function chattingButton({
   className,
   onPress,
 }: {
