7a10d9e760f72dee0a9c2a7ca7aefc2b673f6c56
Son Ahyun <dkgus731@naver.com>
2025-10-11 20:38:01 +0900

    ✨ (#339) Navbar 스크롤 상태에 따른 스타일 및 로고 변경 기능 추가

--- frontend/app/(non-login)/(landing)/_components/Navbar.tsx
+++ frontend/app/(non-login)/(landing)/_components/Navbar.tsx
@@ -1,14 +1,34 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 import styles from "../page.module.scss";
 import { ROUTES } from "@/constants/routes";
 import { IMAGES } from "@/constants/images";
 
 export default function Navbar() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      setIsScrolled(scrollTop > 50);
+    };
+
+    handleScroll();
+
+    window.addEventListener("scroll", handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <nav className={styles.navbar}>
+    <nav className={`${styles.navbar} ${isScrolled ? styles.scrolled : ""}`}>
       <Image
-        src={IMAGES.logo5}
+        src={isScrolled ? IMAGES.logo3 : IMAGES.logo5}
         alt="logo"
         width={200}
         height={100}
