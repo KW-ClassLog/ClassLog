98129b90121e8dba195fc20eb8b9c638acf4ab8a
Son Ahyun <dkgus731@naver.com>
2025-10-11 20:26:08 +0900

    ✨ (#339) StartButtonSection 추가

--- frontend/app/(non-login)/(landing)/page.tsx
+++ frontend/app/(non-login)/(landing)/page.tsx
@@ -6,6 +6,7 @@ import IntroSection from "./_components/IntroSection";
 import Navbar from "./_components/Navbar";
 import styles from "./page.module.scss";
 import RoadMapSection from "./_components/RoadMapSection";
+import { StartButtonSection } from "./_components/StartButtonSection";
 
 export default function LandingPage() {
   const introSectionRef = useRef<HTMLElement>(null);
@@ -17,6 +18,7 @@ export default function LandingPage() {
         <HeroSection introSectionRef={introSectionRef} />
         <IntroSection ref={introSectionRef} />
         <RoadMapSection />
+        <StartButtonSection />
       </div>
     </div>
   );
--- frontend/constants/images.ts
+++ frontend/constants/images.ts
@@ -4,6 +4,7 @@ import logo2 from "@/public/images/logo2.png";
 import logo3 from "@/public/images/logo3.png";
 import logo4 from "@/public/images/logo4.png";
 import logo5 from "@/public/images/logo5.png";
+import logo6 from "@/public/images/logo6.png";
 import introImage from "@/public/images/intro_image.png";
 import qrCode from "@/public/images/QRcode.png";
 import kakaoLogo from "@/public/images/kakao_logo.png";
@@ -29,6 +30,7 @@ export const IMAGES = {
   logo3,
   logo4,
   logo5,
+  logo6,
   introImage,
   qrCode,
   kakaoLogo,
