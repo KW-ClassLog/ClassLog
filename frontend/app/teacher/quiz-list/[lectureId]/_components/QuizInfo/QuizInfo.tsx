6c67f3c73b077f6be364911bf37e50c61604a059
Haemin Kim <haemin1230@gmail.com>
2025-11-12 01:45:59 +0900

    ✨ (#365) 퀴즈 정보 컴포넌트 추가

---
+++ frontend/app/teacher/quiz-list/[lectureId]/_components/QuizInfo/QuizInfo.tsx
@@ -0,0 +1,27 @@
+"use client";
+import React from "react";
+import styles from "./QuizInfo.module.scss";
+
+const data = {
+  title: "Ensemble 1",
+  quizDate: "2025-06-03",
+  quizDay: "화",
+};
+
+function formatDate(date: string, day: string) {
+  const [yyyy, mm, dd] = date.split("-");
+  return `${yyyy}.${mm}.${dd} (${day})`;
+}
+
+export default function QuizInfo() {
+  return (
+    <div className={styles.infoRow}>
+      <div className={styles.title}>
+        [{data.title}] <span className={styles.dashboard}>퀴즈</span>
+      </div>
+      <div className={styles.date}>
+        {formatDate(data.quizDate, data.quizDay)}
+      </div>
+    </div>
+  );
+}
